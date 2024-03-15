f9f87940acf21477dccdc91e22c5e3148d1f7966
mohithchintu <konireddynagamohithreddy@gmail.com>
2024-03-15 18:31:20 +0530

    Fourth commit

--- my-app/app/components/direction.tsx
+++ my-app/app/components/direction.tsx
@@ -13,7 +13,7 @@ export function DirectionHover() {
         <div className="h-[40rem] relative  flex items-center justify-center">
             <DirectionAwareHover imageUrl={imageUrl}>
                 <p className="font-bold text-xl">In the mountains</p>
-                <p className="font-normal text-sm">$1299 / night</p>
+                <p className="font-normal text-sm">Ideal Trip</p>
             </DirectionAwareHover>
         </div>
     );
